0e526558e58a64036e4a3aeecf19787e668a708e
Jason Coleman <jason@strangerstudios.com>
2013-08-29 16:05:38 -0400

    fixed quotes

--- chapter-04/example-28.php
+++ chapter-04/example-28.php
@@ -1,6 +1,6 @@
 <?php
 //load our stylesheet
-wp_enqueue_style('bootstrap', get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap.min.css', ‘style’, '3.0');
+wp_enqueue_style('bootstrap', get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap.min.css', 'style', '3.0');
 
 //and this shows up in the head section of the site (note the 3.0)
 /*
