c09161b2d32cd85396dc34701e5fe48e9239de47
Jason Coleman <jason@strangerstudios.com>
2013-08-29 15:42:16 -0400

    fixed quotes

--- chapter-04/example-06.php
+++ chapter-04/example-06.php
@@ -1,16 +1,16 @@
 <?php
 //schoolpress/shortcodes/invite-students.php
-function sp_invite_students_shortcode($atts, $content=null, $code="")
+function sp_invite_students_shortcode($atts, $content=null, $code='')
 {			
 	//start output buffering
 ob_start();						
 	
 	//look for an invite-students template part in the active theme
-	$template = locate_template(“schoolpress/templates/invite-students.php”);
+	$template = locate_template('schoolpress/templates/invite-students.php');
 	
 	//if not found, use the default
 	if(empty($template))
-		$template = dirname(__FILE__) . “/../templates/invite-students.php”;
+		$template = dirname(__FILE__) . '/../templates/invite-students.php';
 
 	//load the template
 	load_template($template);
@@ -20,4 +20,4 @@ ob_start();
 	ob_end_clean();
 	return $temp_content;			
 }
-add_shortcode("invite-students", "sp_invite_students_shortcode");
+add_shortcode('invite-students', 'sp_invite_students_shortcode');
