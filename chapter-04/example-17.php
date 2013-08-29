d242044e2a02f42f0fdac39d974a344b5f0a5106
Jason Coleman <jason@strangerstudios.com>
2013-08-29 15:44:55 -0400

    fixed quotes

--- chapter-04/example-17.php
+++ chapter-04/example-17.php
@@ -1,9 +1,9 @@
 <?php
 function remove_login_link($classes, $item)
 {
-	f(is_page(‘login’) && $item->title == "Login")
-             	$classes[] = "hide";	//hide this item
+	f(is_page('login') && $item->title == 'Login')
+             	$classes[] = 'hide';	//hide this item
 
      	return $classes;
 }
-add_filter(“nav_menu_css_class”, “sp_nav_menu_css_class”, 10, 2);
+add_filter('nav_menu_css_class', 'sp_nav_menu_css_class', 10, 2);
