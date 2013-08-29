20416616f06211e6d60a6f7ff520daf8f039b262
Jason Coleman <jason@strangerstudios.com>
2013-08-29 16:02:56 -0400

    fixed quotes

--- chapter-04/example-22.php
+++ chapter-04/example-22.php
@@ -1,13 +1,13 @@
 <?php
 function sp_wp_footer_modernizer()
 {
-	wp_enqueue_script(“modernizer”, get_stylesheet_directory_uri() . “/js/modernizer.min.js”);
+	wp_enqueue_script('modernizer', get_stylesheet_directory_uri() . '/js/modernizer.min.js');
 	?>
 	<script>
 		//change search inputs to text if unsupported
 if(!Modernizr.inputtypes.search)
-			jQuery(‘input[type=search]’).attr(‘type’, ‘text’);	
+			jQuery('input[type=search]').attr('type', 'text');	
 	</script>
 	<?php
 }
-add_action(“wp_footer”, “sp_wp_footer_modernizer”);
+add_action('wp_footer', 'sp_wp_footer_modernizer');
