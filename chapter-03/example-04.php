4f7c51b1f79df3c65ea97837adf98e7417abb430
Jason Coleman <jason@strangerstudios.com>
2013-08-29 15:33:18 -0400

    fixed quotes

--- chapter-03/example-04.php
+++ chapter-03/example-04.php
@@ -2,8 +2,8 @@
 function sp_load_styles()
 {
 	if(is_admin())
-		wp_enqueue_style(‘schoolpress-plugin-admin’, plugins_url('css/admin.css',__FILE__ ), array(), SCHOOLPRESS_VERSION, “screen”);
+		wp_enqueue_style('schoolpress-plugin-admin', plugins_url('css/admin.css',__FILE__ ), array(), SCHOOLPRESS_VERSION, 'screen');
 	else
-		wp_enqueue_style(‘schoolpress-plugin-frontend’, plugins_url('css/frontend.css',__FILE__ ), array(), SCHOOLPRESS_VERSION, “screen”);
+		wp_enqueue_style('schoolpress-plugin-frontend', plugins_url('css/frontend.css',__FILE__ ), array(), SCHOOLPRESS_VERSION, 'screen');
 }
-add_action(“init”, “sp_load_styles”);
+add_action('init', 'sp_load_styles');
