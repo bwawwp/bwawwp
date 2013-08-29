5775ce8610c042f45e917cba30fc476b36f87039
Jason Coleman <jason@strangerstudios.com>
2013-08-29 15:32:02 -0400

    fixing quotes

--- chapter-03/example-05.php
+++ chapter-03/example-05.php
@@ -2,8 +2,8 @@
 function sp_load_scripts()
 {
 	if(is_admin())
-		wp_enqueue_script(‘schoolpress-plugin-admin’, plugins_url('js/admin.js',__FILE__ ), array(‘jquery’), SCHOOLPRESS_VERSION);
+		wp_enqueue_script('schoolpress-plugin-admin', plugins_url('js/admin.js',__FILE__ ), array('jquery'), SCHOOLPRESS_VERSION);
 	else
-		wp_enqueue_script(‘schoolpress-plugin-frontend’, plugins_url('js/frontend.js',__FILE__ ), array(‘jquery’), SCHOOLPRESS_VERSION);
+		wp_enqueue_script('schoolpress-plugin-frontend', plugins_url('js/frontend.js',__FILE__ ), array('jquery'), SCHOOLPRESS_VERSION);
 }
-add_action(“init”, “sp_load_scripts”);
+add_action('init', 'sp_load_scripts');
