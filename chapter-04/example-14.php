2157d5e73cec5f1684a3a3c33f454357ab2309a3
Jason Coleman <jason@strangerstudios.com>
2013-08-29 15:43:56 -0400

    fixed quotes

--- chapter-04/example-14.php
+++ chapter-04/example-14.php
@@ -1,7 +1,7 @@
 <?php
 function startbox_child_init()
 {
-	wp_enqueue_style('bootstrap', get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap.min.css', ‘style’, '3.0');
-	wp_enqueue_script('bootstrap', get_stylesheet_directory_uri() . '/bootstrap/js/bootstrap.min.js', ‘jquery’, '3.0');
+	wp_enqueue_style('bootstrap', get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap.min.css', 'style', '3.0');
+	wp_enqueue_script('bootstrap', get_stylesheet_directory_uri() . '/bootstrap/js/bootstrap.min.js', 'jquery', '3.0');
 }
-add_action("init", "startbox_child_init");
+add_action('init', 'startbox_child_init');
