1e91c911d71f5491809df2e11303ea917e088b1b
Jason Coleman <jason@strangerstudios.com>
2013-08-29 16:04:47 -0400

    fixed quotes

--- chapter-04/example-25.php
+++ chapter-04/example-25.php
@@ -5,19 +5,19 @@ function sp_init_browser_hacks()
 	if($is_IE)
 	{
 		//check version and load CSS
-		$user_agent = strtolower($_SERVER[‘HTTP_USER_AGENT’]);
-		if(strpos(“msie 6.”, $user_agent) !== false && 
-		   strpos(“opera”, $user_agent) === false)
+		$user_agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+		if(strpos('msie 6.', $user_agent) !== false && 
+		   strpos('opera', $user_agent) === false)
 		{
-			wp_enqueue_style(‘ie6-hacks’, get_stylesheet_directory_uri() . ‘/css/ie6.css’);
+			wp_enqueue_style('ie6-hacks', get_stylesheet_directory_uri() . '/css/ie6.css');
 		}
 	}
 
 	if(wp_is_mobile())
 	{
 		//load our mobile CSS and JS
-		wp_enqueue_style(‘sp-mobile’, get_stylesheet_directory_uri() . ‘/css/mobile.css’);
-		wp_enqueue_script(‘sp-mobile’, get_stylesheet_directory_uri() . ‘/js/mobile.js’);
+		wp_enqueue_style('sp-mobile', get_stylesheet_directory_uri() . '/css/mobile.css');
+		wp_enqueue_script('sp-mobile', get_stylesheet_directory_uri() . '/js/mobile.js');
 	}
 }
-add_action(“init”, “sp_init_browser_hacks”);
+add_action('init', 'sp_init_browser_hacks');
