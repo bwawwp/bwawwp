d6a6be274ee743c5b5d6e6fa5463330a71fba143
Jason Coleman <jason@strangerstudios.com>
2013-08-29 15:39:04 -0400

    fixed quotes

--- chapter-03/example-11.php
+++ chapter-03/example-11.php
@@ -1,20 +1,20 @@
 <?php
 global $wpdb;
-$user_query = $_REQUEST[‘uq’];
+$user_query = $_REQUEST['uq'];
 
-$sqlQuery = “SELECT user_login FROM $wpdb->users WHERE 
-user_login LIKE ‘%” . $wpdb->escape($user_query) . “%’ OR
-user_email LIKE ‘%” . $wpdb->escape($user_query) . “%’ OR
-display_name LIKE ‘%” . $wpdb->escape($user_query) . “%’
-”;
+$sqlQuery = "SELECT user_login FROM $wpdb->users WHERE 
+	user_login LIKE '%" . $wpdb->escape($user_query) . "%' OR
+	user_email LIKE '%" . $wpdb->escape($user_query) . "%' OR
+	display_name LIKE '%" . $wpdb->escape($user_query) . "%'
+	";
 $user_logins = $wpdb->get_col($sqlQuery);
 
 if(!empty($user_logins))
 {
-	echo “<ul>”;
-foreach($user_logins as $user_login)
+	echo "<ul>";
+	foreach($user_logins as $user_login)
 	{
-		echo “<li>$user_login</li>”;
-}
-echo “</ul>”;
+		echo "<li>$user_login</li>";
+	}
+	echo "</ul>";
 }
