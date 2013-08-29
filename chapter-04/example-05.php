809e908e0f56af619ed7a11852a2a4ad798ad105
Jason Coleman <jason@strangerstudios.com>
2013-08-29 15:41:11 -0400

    fixed quotes

--- chapter-04/example-05.php
+++ chapter-04/example-05.php
@@ -2,8 +2,8 @@
 //schoolpress/templates/invite-students.php
 ?>
 <p>Enter</p>
-<form action=”” method=”post”>
-        <label for=”email”>Email:</label>
-<input type=”text” id=”email” name=”email” value=”” />
-        <input type=”submit” name=”invite” value=”Invite Student” />
+<form action="" method="post">
+        <label for="email">Email:</label>
+<input type="text" id="email" name="email" value="" />
+        <input type="submit" name="invite" value="Invite Student" />
 </form>
