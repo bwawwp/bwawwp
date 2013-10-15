0a6e1cf85c66dfa03c262f0494fd61af50457664
Jason Coleman <jason@strangerstudios.com>
2013-10-15 17:04:01 -0400

    Create example-13.php

---
+++ chapter-09/example-13.php
@@ -0,0 +1,8 @@
+<div>
+    Submitted:
+    <span id="assignment_count">
+        <?php echo count($assignment->submissions);?>
+    </span>
+    /
+    <?php echo count($course->students);?>
+</div>
