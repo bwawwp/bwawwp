6a8c70ef2e034b8e60077b42d9c64902df757a7d
Jason Coleman <jason@strangerstudios.com>
2013-10-15 17:02:04 -0400

    Create example-07.js

---
+++ chapter-09/example-07.js
@@ -0,0 +1,21 @@
+//Option #1: Disabling a button while an AJAX request is processing
+jQuery('#button').click(function() {    
+    //disable the button
+    jQuery(this).attr('disabled', 'disabled');
+    
+    //do the ajax request
+    jQuery.ajax({
+        url: ajaxurl,type:'GET',timeout:5000,
+    	dataType: 'html',    	
+    	error: function(xml){
+    		//error stuff
+    	},
+    	success: function(response){
+            //success stuff                        
+    	}
+        complete: function() {
+            //enable the button again
+            jQuery('#button').removeAttr('disabled');
+        }
+    });
+});
