5012a6743b05c89053427b3d1cbe01815809b445
Jason Coleman <jason@strangerstudios.com>
2013-10-15 17:00:04 -0400

    Create example-03.php

---
+++ chapter-09/example-03.php
@@ -0,0 +1,14 @@
+//frontend JS
+function sp_wp_enqueue_scripts()
+{
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('schoolpress-plugin-frontend', plugins_url('js/frontend.js', __FILE__), array('jquery'), '1.0');    
+}
+add_action("wp_enqueue_scripts", "sp_wp_enqueue_scripts");
+
+//admin JS
+function sp_admin_enqueue_scripts()
+{
+    wp_enqueue_script('schoolpress-plugin-admin', plugins_url('js/admin.js', __FILE__), array('jquery'), '1.0');	
+}
+add_action('admin_enqueue_scripts', 'sp_admin_enqueue_scripts');
