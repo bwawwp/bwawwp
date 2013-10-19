280532166d8b054eff46f98cf7c85e37f28ae163
Brian Messenlehner <brian@webdevstudios.com>
2013-10-19 17:09:07 -0400

    no message

--- chapter-14/example-10.php
+++ chapter-14/example-10.php
@@ -1,5 +1,5 @@
 <?php
-//Gets the full URL for an image given the image filename.
+// Gets the full URL for an image given the image filename.
 function schoolpress_get_image( $image ) {
 	$dir = apply_filters( 'schoolpress_images_url', SCHOOLPRESS_URL . '/images/' );
 	return $dir . $image;
--- chapter-14/example-2.php
+++ chapter-14/example-2.php
@@ -1,7 +1,7 @@
 <?php
-//setting a variable to a string without localization
+// setting a variable to a string without localization
 $title = 'Assignments';
 
-//setting a variable to a string with localization
+// setting a variable to a string with localization
 $title = __( 'Assignments', 'schoolpress' );
 ?>
