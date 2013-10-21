c1e008eb44d7c4f7a66efc732617ffd7cc99847f
Jason Coleman <jason@strangerstudios.com>
2013-10-20 20:14:04 -0700

    Update example-04.php

--- chapter-07/example-04.php
+++ chapter-07/example-04.php
@@ -1,15 +1,15 @@
 <?php
-  //make a copy of the original shortcodes
-  global $shortcode_tags;
-  $original_shortcode_tags = $shortcode_tags;
-  
-  //remove the [msg] shortcode
-  unset($shortcode_tags['msg']);
-  
-  //do shortcodes to support nested shortcodes of other types
-  $content = do_shortcode($content);
-  echo $content;
-  
-  //restore the original shortcodes
-  $shortcode_tags = $original_shortcode_tags;
+//make a copy of the original shortcodes
+global $shortcode_tags;
+$original_shortcode_tags = $shortcode_tags;
+
+//remove the [msg] shortcode
+unset($shortcode_tags['msg']);
+
+//do shortcodes and echo
+$content = do_shortcode($content);
+echo $content;
+
+//restore the original shortcodes
+$shortcode_tags = $original_shortcode_tags;
 ?>
