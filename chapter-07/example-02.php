2d6e1073df398222c6b84ed18330a1b020e8889a
Jason Coleman <jason@strangerstudios.com>
2013-10-20 18:45:09 -0700

    Create example-02.php

---
+++ chapter-07/example-02.php
@@ -0,0 +1,13 @@
+<?php
+global $post;
+$sidebar_content = $post->sidebar_content;
+?>
+<div class="post">
+  <?php the_content(); ?>
+</div>
+<div class="sidebar">
+  <?php
+    //echo do_shortcode($sidebar_content);
+    echo apply_filters("the_content", $sidebar_content);
+  ?>
+</div>
