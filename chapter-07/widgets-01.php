658e450f97fbf3bdbe793c45bfb9f1d392cf1bf6
Jason Coleman <jason@strangerstudios.com>
2013-10-24 14:55:22 -0400

    Create widgets-01.php

---
+++ chapter-07/widgets-01.php
@@ -0,0 +1,24 @@
+<?php
+/*
+  Taken from the Widgets API Codex Page at:
+  http://codex.wordpress.org/Widgets_API
+*/
+class My_Widget extends WP_Widget {
+
+	public function __construct() {
+		// widget actual processes
+	}
+
+	public function widget( $args, $instance ) {
+		// outputs the content of the widget
+	}
+
+ 	public function form( $instance ) {
+		// outputs the options form on admin
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		// processes widget options to be saved
+	}
+}
+?>
