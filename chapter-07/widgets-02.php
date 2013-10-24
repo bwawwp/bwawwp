7aca9316fa98dbc779df581b9dc2a74402e73bb1
Jason Coleman <jason@strangerstudios.com>
2013-10-24 15:19:43 -0400

    Update widgets-02.php

--- chapter-07/widgets-02.php
+++ chapter-07/widgets-02.php
@@ -1,4 +1,8 @@
 <?php
+/*
+  Taken from the Widgets API Codex Page at:
+  http://codex.wordpress.org/Widgets_API
+*/
 add_action('widgets_init',
      create_function('', 'return register_widget("My_Widget");')
 );
