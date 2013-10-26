af9995968a2a194d7c99005cced23645dc5e09b1
Jason Coleman <jason@strangerstudios.com>
2013-10-26 13:58:46 -0400

    Create widgets-04.php

---
+++ chapter-07/widgets-04.php
@@ -0,0 +1,10 @@
+<?php
+register_sidebar(array(
+  'name' => 'Student Status',
+  'id' => 'schoolpress_student_status',
+  'before_widget' => '',
+  'after_widget' => '',
+  'before_title' => '',
+  'after_title' => ''
+));
+?>
