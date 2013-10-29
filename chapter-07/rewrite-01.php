f840b20a464aa850edb644c3714394b03a7be4a9
Jason Coleman <jason@strangerstudios.com>
2013-10-29 13:44:51 -0400

    Create rewrite-01.php

---
+++ chapter-07/rewrite-01.php
@@ -0,0 +1,34 @@
+<?php
+//Add rule and flush on acitvation.
+function sp_activation()
+{
+	add_rewrite_rule(
+	  '/contact/([^/]+)/?',
+	  'index.php?name=contact&subject=' . $matches[1],
+	  'top'
+	);
+	flush_rewrite_rules();
+}
+register_activation_hook('sp_activation');
+
+/*
+  Add rule on init in case another plugin flushes,
+  but don't flush cause it's expensive
+*/
+function sp_init()
+{
+	add_rewrite_rule(
+	  '/contact/([^/]+)/?',
+	  'index.php?name=contact&subject=' . $matches[1],
+	  'top'
+	);
+}
+add_action('init', 'sp_init');
+
+//Fush rewrite rules on deactivation to remove our rule.
+function sp_deactivation()
+{
+	flush_rewrite_rules();
+}
+register_deactivation_hook('sp_deactivation');
+?>
