e03c6cc37b641acd2884458d7a40b0d380c7540b
Jason Coleman <jason@strangerstudios.com>
2013-10-30 13:30:28 -0400

    Update settings-03.php

--- chapter-07/settings-03.php
+++ chapter-07/settings-03.php
@@ -1,10 +1,10 @@
 <?php
 //require any user account
-add_action('wpdoc_caps', function($caps) { return array('read'); });
+add_filter('wpdoc_caps', function($caps) { return array('read'); });
 
 //require admin account
-add_action('wpdoc_caps', function($caps) { return array('manage_options'); });
+add_filter('wpdoc_caps', function($caps) { return array('manage_options'); });
 
 //authors only or users with a custom capability (doc)
-add_action('wpdoc_caps', function($caps) { return array('edit_post', 'doc'); });
+add_filter('wpdoc_caps', function($caps) { return array('edit_post', 'doc'); });
 ?>
