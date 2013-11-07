4b61e6cdb2e45ad8cbb712abda3297cfc30a3420
Jason Coleman <jason@strangerstudios.com>
2013-11-07 14:06:50 -0500

    Update example-03.php

--- chapter-15/example-03.php
+++ chapter-15/example-03.php
@@ -26,7 +26,6 @@ function always_https_redirect()
 	}
 }
 add_action('wp', 'always_https_redirect', 2);
-add_action('login_init', 'always_https_redirect', 2);
 
 //(optional) Tell Paid Memberships Pro to get on board with the HTTPS redirect.
 add_filter("pmpro_besecure", "__return_true");
