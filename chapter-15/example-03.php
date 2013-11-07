0aa8e8b9569a1eff602022d45a38338903d96c40
Jason Coleman <jason@strangerstudios.com>
2013-11-07 14:04:50 -0500

    Create example-03.php

---
+++ chapter-15/example-03.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Always HTTPS
+Plugin URI: http://www.strangerstudios.com/wp/always-https
+Description: Redirect all URLs to the HTTPS version.
+Version: .1
+Author: strangerstudios
+*/
+
+//redirect to https
+function always_https_redirect()
+{
+	//if FORCE_SSL_ADMIN is true and we're not over HTTPS
+	if(force_ssl_admin() && !is_ssl())
+	{
+		//redirect to https version of the page
+		wp_redirect("https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+		exit;
+	}
+}
+add_action('wp', 'always_https_redirect', 2);
+add_action('login_init', 'always_https_redirect', 2);
+
+//(optional) Tell Paid Memberships Pro to get on board with the HTTPS redirect.
+add_filter("pmpro_besecure", "__return_true");
+?>
