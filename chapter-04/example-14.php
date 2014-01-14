1cc483b84d65aa13e7561b3c90b16187c6ac6b08
Jason Coleman <jason@strangerstudios.com>
2014-01-14 18:03:37 -0500

    Update example-14.php
    linebreaks for print

--- chapter-04/example-14.php
+++ chapter-04/example-14.php
@@ -2,13 +2,15 @@
 function startbox_child_init() {
 	wp_enqueue_style( 
 		'bootstrap', 
-		get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap.min.css', 
+		get_stylesheet_directory_uri() . 
+			'/bootstrap/css/bootstrap.min.css', 
 		'style', 
 		'3.0' 
 	);
 	wp_enqueue_script( 
 		'bootstrap', 
-		get_stylesheet_directory_uri() . '/bootstrap/js/bootstrap.min.js', 
+		get_stylesheet_directory_uri() . 
+			'/bootstrap/js/bootstrap.min.js', 
 		'jquery', 
 		'3.0' 
 	);
