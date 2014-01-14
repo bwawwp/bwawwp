4d798bd2cbf937bd471ae4477fb3b829de0802d8
Jason Coleman <jason@strangerstudios.com>
2014-01-14 18:07:38 -0500

    Update example-20.php

--- chapter-04/example-20.php
+++ chapter-04/example-20.php
@@ -1,5 +1,5 @@
-@media(-o-min-device-pixel-ratio: 5/4),			/* Opera */
+@media(-o-min-device-pixel-ratio: 5/4),		/* Opera */
 	(-webkit-min-device-pixel-ratio: 1.25), 	/* Webkit */
-	(min-resolution: 120dpi) {			/* Others */
+	(min-resolution: 120dpi) {		/* Others */
 		/* add your high res CSS here */
 	}
