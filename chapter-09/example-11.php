a8067d547301c263875c66cfde12369edc7a64c0
Jason Coleman <jason@strangerstudios.com>
2014-01-15 04:32:26 -0500

    Update example-11.php
    linebreaks for print

--- chapter-09/example-11.php
+++ chapter-09/example-11.php
@@ -5,28 +5,31 @@ function hbdemo_wp_footer()
 ?>
 <script>
   jQuery(document).ready(function() {    			
-		//hook into heartbeat-send: client will send the message 
-		//'marco' in the 'client' var inside the data array
-		jQuery(document).on('heartbeat-send', function(e, data) {
-			console.log('Client: marco');
-			data['client'] = 'marco';	//need some data to kick off AJAX call
-		});
+	//hook into heartbeat-send: client will send the message 
+	//'marco' in the 'client' var inside the data array
+	jQuery(document).on('heartbeat-send', function(e, data) {
+		console.log('Client: marco');
 		
-		//hook into heartbeat-tick: client looks for a 'server' 
-		//var in the data array and logs it to console
-		jQuery(document).on('heartbeat-tick', function(e, data) {			
-			if(data['server'])
-				console.log('Server: ' + data['server']);
-		});
-				
-		//hook into heartbeat-error: in case of error, let's log some stuff
-		jQuery(document).on('heartbeat-error', function(e, jqXHR, textStatus, error) {
+		//need some data to kick off AJAX call
+		data['client'] = 'marco';
+	});
+	
+	//hook into heartbeat-tick: client looks for a 'server' 
+	//var in the data array and logs it to console
+	jQuery(document).on('heartbeat-tick', function(e, data) {			
+		if(data['server'])
+			console.log('Server: ' + data['server']);
+	});
+			
+	//hook into heartbeat-error to log errors
+	jQuery(document).on('heartbeat-error', 
+		function(e, jqXHR, textStatus, error) {
 			console.log('BEGIN ERROR');
 			console.log(textStatus);
 			console.log(error);			
 			console.log('END ERROR');			
 		});
-	});		
+  });		
 </script>
 <?php
 }
