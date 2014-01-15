5bb9d50cd7eea022c493651a8f7b2b3f64900705
Jason Coleman <jason@strangerstudios.com>
2014-01-15 04:40:44 -0500

    Update example-16.php
    line breaks for print

--- chapter-09/example-16.php
+++ chapter-09/example-16.php
@@ -2,14 +2,19 @@
 //processing the message on the server
 function sp_heartbeat_received_assignment_count($response, $data)
 {
-    //check for assignment post id
-    if(!empty($data['schoolpress']['assignment_post_id']))
+	//check for assignment post id
+	if(!empty($data['schoolpress']['assignment_post_id']))
 	{
-        $assignment = new Assignment($data['schoolpress']['assignment_post_id']);
-        $response['schoolpress']['assignment_count'] = count($assignment->submissions);
-    }
+		$assignment = new Assignment(
+			$data['schoolpress']['assignment_post_id']
+			);
+		$response['schoolpress']['assignment_count'] = count(
+			$assignment->submissions
+			);
+	}
 	
 	return $response;
 }
-add_filter('heartbeat_received', 'sp_heartbeat_received_assignment_count', 10, 2);
+add_filter('heartbeat_received', 
+	'sp_heartbeat_received_assignment_count', 10, 2);
 ?>
