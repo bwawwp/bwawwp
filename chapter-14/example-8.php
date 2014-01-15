34be10c7ecf8c706ded5b57373aa08ba93309d2a
Jason Coleman <jason@strangerstudios.com>
2014-01-15 04:48:17 -0500

    Update example-8.php
    pulled in updated version from Atlas

--- chapter-14/example-8.php
+++ chapter-14/example-8.php
@@ -1,18 +1,22 @@
 <?php
 function schoolpress_load_textdomain() {
-	// get the locale
-	$locale = apply_filters( 'plugin_locale', get_locale(), 'schoolpress' );
-	$mofile = 'schoolpress-' . $locale . '.mo';
+    // get the locale
+    $locale = apply_filters( 'plugin_locale', get_locale(), 'schoolpress' );
+    $mofile = 'schoolpress-' . $locale . '.mo';
 
-	// paths to local (plugin) and global (WP) language files
-	$mofile_local  = dirname( plugin_basename( __FILE__ ) ).'/languages/' . $mofile;
-	$mofile_global = WP_LANG_DIR . '/schoolpress/' . $mofile;
+    /*
+    Paths to local (plugin) and global (WP) language files.
+    Note: dirname(__FILE__) here changes if this code 
+    is placed outside the base plugin file.
+    */
+    $mofile_local  = dirname( __FILE__ ).'/languages/' . $mofile;
+    $mofile_global = WP_LANG_DIR . '/schoolpress/' . $mofile;
 
-	// load global first
-	load_textdomain( 'schoolpress', $mofile_global );
+    // load global first
+    load_textdomain( 'schoolpress', $mofile_global );
 
-	// load local second
-	load_textdomain( 'schoolpress', $mofile_local );
+    // load local second
+    load_textdomain( 'schoolpress', $mofile_local );
 }
 add_action( 'init', 'schoolpress_load_textdomain', 1 );
 ?>
