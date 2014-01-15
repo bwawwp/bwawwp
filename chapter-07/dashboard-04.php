46a8b38378ac52832436842d15d81cde0c460f7b
Jason Coleman <jason@strangerstudios.com>
2014-01-14 19:17:11 -0500

    Update dashboard-04.php
    linebreaks for print

--- chapter-07/dashboard-04.php
+++ chapter-07/dashboard-04.php
@@ -18,12 +18,16 @@ add_action( 'wp_dashboard_setup', 'sp_add_dashboard_widgets' );
 //widget
 function sp_assignments_dashboard_widget() {
 	$options = get_option( "assignments_dashboard_widget_options", array() );
-
-	if ( !empty( $options['course_id'] ) )
-		$group = groups_get_group( array( 'group_id'=>$options['course_id'] ) );
-
+	
+	if ( !empty( $options['course_id'] ) ) {
+		$group = groups_get_group( array( 
+			'group_id'=>$options['course_id'] 
+			) );
+	}
+	
 	if ( !empty( $group ) ) {
-		echo "Showing assignments for class " . $group->name . ".<br />...";
+		echo "Showing assignments for class " . 
+			$group->name . ".<br />...";
 		/*
 			get assignments for this group and list their status
 		*/
@@ -43,7 +47,9 @@ function sp_assignments_dashboard_widget_configuration() {
 	//saving options?
 	if ( isset( $_POST['assignments_dashboard_options_save'] ) ) {
 		//get course_id
-		$options['course_id'] = intval( $_POST['assignments_dashboard_course_id'] );
+		$options['course_id'] = intval( 
+			$_POST['assignments_dashboard_course_id'] 
+			);
 
 		//save it
 		update_option( "assignments_dashboard_widget_options", $options );
@@ -56,22 +62,24 @@ function sp_assignments_dashboard_widget_configuration() {
 	<div class="feature_post_class_wrap">
 		<label>Class</label>
 		<select name="assignments_dashboard_course_id">
-			<option value="" <?php selected( $options['course_id'], "" );?>>
-				All Classes
+		<option value="" <?php selected( $options['course_id'], "" );?>>
+			All Classes
+		</option>
+		<?php
+		$groups = groups_get_groups( array( 'orderby'=>'name', 
+						'order'=>'ASC' ) );
+		
+		if ( !empty( $groups ) && !empty( $groups['groups'] ) ) {
+			foreach ( $groups['groups'] as $group ) {
+			?>
+			<option value="<?php echo intval( $group->id );?>" 
+			<?php selected( $options['course_id'], $group->id );?>>
+			<?php echo $group->name;?>
 			</option>
 			<?php
-			$groups = groups_get_groups( array( 'orderby'=>'name', 'order'=>'ASC' ) );
-			if ( !empty( $groups ) && !empty( $groups['groups'] ) ) {
-				foreach ( $groups['groups'] as $group ) {
-				?>
-				<option value="<?php echo intval( $group->id );?>" 
-					<?php selected( $options['course_id'], $group->id );?>>
-					<?php echo $group->name;?>
-				</option>
-				<?php
-				}
 			}
-			?>
+		}
+		?>
 		</select>
 	</div>
 	<input type="hidden" name="assignments_dashboard_options_save" value="1" />
