422de2ae5c8ee3c2bd2cfb6dd0f1dce616417438
Jason Coleman <jason@strangerstudios.com>
2014-01-15 04:50:17 -0500

    Update example-9.php
    line breaks for print

--- chapter-14/example-9.php
+++ chapter-14/example-9.php
@@ -1,6 +1,8 @@
 <?php
 function schoolpress_load_textdomain() {
-	load_theme_textdomain( 'schoolpress', get_template_directory() . '/languages/' );
+	load_theme_textdomain( 
+		'schoolpress', get_template_directory() . '/languages/' 
+		);
 }
 add_action( 'init', 'schoolpress_load_textdomain', 1 );
 ?>
