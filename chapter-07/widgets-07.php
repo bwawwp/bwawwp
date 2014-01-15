3c87643e0440a9e10b572f89f8ca347f972561fa
Jason Coleman <jason@strangerstudios.com>
2014-01-14 19:07:04 -0500

    Update widgets-07.php
    linebreaks/etc for print

--- chapter-07/widgets-07.php
+++ chapter-07/widgets-07.php
@@ -6,109 +6,107 @@
 */
 class SchoolPress_Note_Widget extends WP_Widget
 {
+  public function __construct() {
+    parent::__construct(
+	'schoolpress_note',
+	'SchoolPress Note',
+	array( 'description' => 'Note to Show on Group Pages' );
+  }
 
-	public function __construct() {
-		parent::__construct(
-			'schoolpress_note',
-			'SchoolPress Note',
-			array( 'description' => 'Note to Show on Group Pages' )
+  public function widget( $args, $instance ) {
+	global $current_user;
+
+	//saving a note edit?
+	if ( !empty( $_POST['schoolpress_note_text'] ) 
+		&& !empty( $_POST['class_id'] ) ) {
+	  //make sure this is an admin
+	  if(groups_is_user_admin($current_user->ID,intval($_POST['class_id']))){
+		//should escape the text and possibly use a nonce
+		update_option(
+			'schoolpress_note_' . intval( $_POST['class_id'] ),
+			$_POST['schoolpress_note_text']
 		);
+	  }
 	}
 
-	public function widget( $args, $instance ) {
-		global $current_user;
+	//look for a global note
+	$note = $instance['note'];
 
-		//saving a note edit?
-		if ( !empty( $_POST['schoolpress_note_text'] ) 
-			&& !empty( $_POST['class_id'] ) ) {
-			//make sure this is an admin
-			if(groups_is_user_admin($current_user->ID,intval($_POST['class_id']))){
-				//should escape the text and possibly use a nonce
-				update_option(
-					'schoolpress_note_' . intval( $_POST['class_id'] ),
-					$_POST['schoolpress_note_text']
-				);
-			}
-		}
+	//get class id for this group
+	$class_id = bp_get_current_group_id();
 
-		//look for a global note
-		$note = $instance['note'];
-
-		//get class id for this group
-		$class_id = bp_get_current_group_id();
-
-		//look for a class note
-		if ( empty( $note ) && !empty( $class_id ) ) {
-			$note = get_option( "schoolpress_note_" . $class_id );
-		}
-
-		//display note
-		if ( !empty( $note ) ) {
-			?>
-			<div id="schoolpress_note">
-				<?php echo wpautop( $note );?>
-			</div>
-			<?php
-
-			//show edit for group admins
-			if ( groups_is_user_admin( $current_user->ID, $class_id ) ) {
-			?>
-			<a id="schoolpress_note_edit_trigger">Edit</a>
-			<div id="schoolpress_note_edit" style="display: none;">
-			<form action="" method="post">
-			<input type="hidden" 
-				name="class_id" 
-				value="<?php echo intval($class_id);?>" />
-			<textarea name="schoolpress_note_text" cols="30" rows="5">
-			<?php echo esc_textarea(get_option('schoolpress_note_'.$class_id));?>
-			</textarea>
-			<input type="submit" value="Save" />
-			<a id="schoolpress_note_edit_cancel" href="javascript:void(0);">
-				Cancel
-			</a>
-			</form>
-			</div>
-			<script>
-				jQuery(document).ready(function() {
-					jQuery('#schoolpress_note_edit_trigger').click(function(){
-						jQuery('#schoolpress_note').hide();
-						jQuery('#schoolpress_note_edit').show();
-					});
-					jQuery('#schoolpress_note_edit_cancel').click(function(){
-						jQuery('#schoolpress_note').show();
-						jQuery('#schoolpress_note_edit').hide();
-					});
-				});
-			</script>
-			<?php
-			}
-		}
+	//look for a class note
+	if ( empty( $note ) && !empty( $class_id ) ) {
+		$note = get_option( "schoolpress_note_" . $class_id );
 	}
 
-	public function form( $instance ) {
-		if ( isset( $instance['note'] ) )
-			$note = $instance['note'];
-		else
-			$note = "";
+	//display note
+	if ( !empty( $note ) ) {
 		?>
-		<p>
-			<label for="<?php echo $this->get_field_id( 'note' ); ?>">
-				<?php _e( 'Note:' ); ?>
-			</label>
-			<textarea id="<?php echo $this->get_field_id( 'note' ); ?>"
-				name="<?php echo $this->get_field_name( 'note' ); ?>">
-				<?php echo esc_textarea( $note );?>
-			</textarea>
-		</p>
+		<div id="schoolpress_note">
+			<?php echo wpautop( $note );?>
+		</div>
 		<?php
+	
+		//show edit for group admins
+		if ( groups_is_user_admin( $current_user->ID, $class_id ) ) {
+		?>
+		<a id="schoolpress_note_edit_trigger">Edit</a>
+		<div id="schoolpress_note_edit" style="display: none;">
+		<form action="" method="post">
+		<input type="hidden" 
+			name="class_id" 
+			value="<?php echo intval($class_id);?>" />
+		<textarea name="schoolpress_note_text" cols="30" rows="5">
+		<?php echo esc_textarea(get_option('schoolpress_note_'.$class_id));?>
+		</textarea>
+		<input type="submit" value="Save" />
+		<a id="schoolpress_note_edit_cancel" href="javascript:void(0);">
+			Cancel
+		</a>
+		</form>
+		</div>
+		<script>
+		jQuery(document).ready(function() {
+			jQuery('#schoolpress_note_edit_trigger').click(function(){
+				jQuery('#schoolpress_note').hide();
+				jQuery('#schoolpress_note_edit').show();
+			});
+			jQuery('#schoolpress_note_edit_cancel').click(function(){
+				jQuery('#schoolpress_note').show();
+				jQuery('#schoolpress_note_edit').hide();
+			});
+		});
+		</script>
+		<?php
+		}
 	}
+  }
 
-	public function update( $new_instance, $old_instance ) {
-		$instance = array();
-		$instance['note'] = $new_instance['note'];
+  public function form( $instance ) {
+	if ( isset( $instance['note'] ) )
+		$note = $instance['note'];
+	else
+		$note = "";
+	?>
+	<p>
+		<label for="<?php echo $this->get_field_id( 'note' ); ?>">
+			<?php _e( 'Note:' ); ?>
+		</label>
+		<textarea id="<?php echo $this->get_field_id( 'note' ); ?>"
+			name="<?php echo $this->get_field_name( 'note' ); ?>">
+			<?php echo esc_textarea( $note );?>
+		</textarea>
+	</p>
+	<?php
+  }
 
-		return $instance;
-	}
+  public function update( $new_instance, $old_instance ) {
+	$instance = array();
+	$instance['note'] = $new_instance['note'];
+
+	return $instance;
+  }
 }
 add_action( 'widgets_init', function() {
 		register_widget( 'SchoolPress_Note_Widget' );
