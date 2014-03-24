f384e692b1f4677e5d69a72561a9c413a56555aa
Jason Coleman <jason@strangerstudios.com>
2014-03-24 10:57:54 -0400

    Update example-11.php

--- chapter-05/example-11.php
+++ chapter-05/example-11.php
@@ -9,15 +9,15 @@ if ( Student::is_student() ) {
 
 	//display info and links
 	if ( !empty( $assignment ) ) {
-?>
-		<p>Your next assignment
-		<a href="<?php echo get_permalink( $assignment->id );?>">
-		<?php echo $assignment->title;?></a>
-		for the 
-		<a href="<?php echo get_permalink( $assignment->class_id );?>">
-		<?php echo $assignment->class->title;?></a>
-		class is due on <?php echo $assignment->getDueDate();?>.</p>
-		<?php
+	?>
+	<p>Your next assignment
+	<a href="<?php echo get_permalink( $assignment->id );?>">
+	<?php echo $assignment->title;?></a>
+	for the 
+	<a href="<?php echo get_permalink( $assignment->class_id );?>">
+	<?php echo $assignment->class->title;?></a>
+	class is due on <?php echo $assignment->getDueDate();?>.</p>
+	<?php
 	}
 }
 ?>
